bd92811b28a4445bee030729fc640d8ff28d86e8
Anisuzzaman Titas <titas@mazegeek.com>
2020-02-25 20:04:45 +0600

    head

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'portfoliohub_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'portfoliohub' ),
+			'primary_menu' => esc_html__( 'Primary Menu', 'portfoliohub' ),
 		) );
 
 		/*
@@ -58,6 +58,10 @@ if ( ! function_exists( 'portfoliohub_setup' ) ) :
 			'gallery',
 			'caption',
 		) );
+        add_theme_support( 'post-formats', array(
+            'audio',
+            'video'
+        ) );
 
 		// Set up the WordPress core custom background feature.
 		add_theme_support( 'custom-background', apply_filters( 'portfoliohub_custom_background_args', array(
